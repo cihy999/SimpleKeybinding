26cec05ec408d0f4a8ebe9cad2865675bb738867
cihy999 <cihy999@gmail.com>
2024-09-07 11:32:06 +0800

    feat: 設置專案

---
+++ Source/SimpleKeybinding/Public/SimpleKeybindingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleKeybindingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASimpleKeybindingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASimpleKeybindingGameMode();
+};
+
+
+
---
+++ Source/SimpleKeybinding/SimpleKeybinding.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SimpleKeybinding : ModuleRules
+{
+	public SimpleKeybinding(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
